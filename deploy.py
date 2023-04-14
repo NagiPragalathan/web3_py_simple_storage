01825515e83f113bc5ef72ccc9b3948e0bd92e74
NagiPragalathan <kk2069037@gmail.com>
2023-04-14 19:48:29 +0530

    contract are runned with abi from meta mask deployed abi and contract address deployed from metamask

--- deploy.py
+++ deploy.py
@@ -43,10 +43,96 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
     "bytecode"
 ]["object"]
 
-# get abi
-abi = json.loads(
-    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
-)["output"]["abi"]
+# get abi { abi from meta mask deployed abi }
+abi = [
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_name",
+                                "type": "string"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_favoriteNumber",
+                                "type": "uint256"
+            }
+        ],
+        "name": "addPerson",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+                "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_favoriteNumber",
+                                "type": "uint256"
+            }
+        ],
+        "name": "store",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+                "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                                "type": "string"
+            }
+        ],
+        "name": "nameToFavoriteNumber",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                                "type": "uint256"
+            }
+        ],
+        "name": "people",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "favoriteNumber",
+                "type": "uint256"
+            },
+            {
+                "internalType": "string",
+                "name": "name",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "retrieve",
+                "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
 
 # w3 = Web3(Web3.HTTPProvider(os.getenv("SEPOLIA_RPC_URL")))
 # chain_id = 4
